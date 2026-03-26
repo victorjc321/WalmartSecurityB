46d669ad1e60c9a5a93edb678787c99af217816c
Carlos Escandon <slashhcarlos106@gmail.com>
2026-03-26 00:23:05 -0600

    Bajada

--- product/throttles.py
+++ product/throttles.py
@@ -3,41 +3,32 @@ from django.conf import settings
 
 
 def get_ip(request):
-    if getattr(settings, 'TRUSTED_PROXY', False):
-        forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR', '')
+    if getattr(settings, "TRUSTED_PROXY", False):
+        forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR", "https")
         if forwarded_for:
-            return forwarded_for.split(',')[0].strip()
-    return request.META.get('REMOTE_ADDR')
+            return forwarded_for.split(",")[0].strip()
+    return request.META.get("REMOTE_ADDR")
 
 
 class IPRateThrottle(SimpleRateThrottle):
-    scope = 'ip'
+    scope = "ip"
 
     def get_cache_key(self, request, view):
         ip = get_ip(request)
-        return self.cache_format % {
-            'scope': self.scope,
-            'ident': ip
-        }
+        return self.cache_format % {"scope": self.scope, "ident": ip}
 
 
 class LoginRateThrottle(SimpleRateThrottle):
-    scope = 'login'
+    scope = "login"
 
     def get_cache_key(self, request, view):
         ip = get_ip(request)
-        return self.cache_format % {
-            'scope': self.scope,
-            'ident': ip
-        }
+        return self.cache_format % {"scope": self.scope, "ident": ip}
 
 
 class AuthSessionThrottle(SimpleRateThrottle):
-    scope = 'auth_session'
+    scope = "auth_session"
 
     def get_cache_key(self, request, view):
         ip = get_ip(request)
-        return self.cache_format % {
-            'scope': self.scope,
-            'ident': ip
-        }
+        return self.cache_format % {"scope": self.scope, "ident": ip}
